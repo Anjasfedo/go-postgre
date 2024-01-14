b6c177cf0f757fa2e4a9284b4d20646adc386917
AnjasFedo <fedoaf98@gmail.com>
2024-01-14 11:18:58 +0700

    Create Connection with Database

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,8 +1,40 @@
 package middleware
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
+
+type Response struct {
+	ID      int64  `json:"id,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+func CreateConnection() *sql.DB {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error Loading .env")
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to Postgres")
+
+	return db
+}
 
 func GetStocks(w http.ResponseWriter, r *http.Request) {
 
