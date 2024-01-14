a6c2c464a07acfd34a166e2c1d4202c57305bd36
AnjasFedo <fedoaf98@gmail.com>
2024-01-14 11:02:46 +0700

    Define Router

---
+++ middleware/middleware.go
@@ -0,0 +1,25 @@
+package middleware
+
+import (
+	"net/http"
+)
+
+func GetStocks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetStockById(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateStock(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateStockById(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteStockById(w http.ResponseWriter, r *http.Request) {
+
+}
--- routers/router.go
+++ routers/router.go
@@ -1,5 +1,20 @@
 package routers
 
 import (
-
+	"github.com/Anjasfedo/go-postgres/middleware"
+	"github.com/gorilla/mux"
 )
+
+func Router() *mux.Router {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api/stock", middleware.GetStocks).Methods("GET", "OPTIONS")
+
+	r.HandleFunc("/api/stock/{id}", middleware.GetStockById).Methods("GET", "OPTIONS")
+
+	r.HandleFunc("/api/stock", middleware.CreateStock).Methods("POST", "OPTIONS")
+
+	r.HandleFunc("/api/stock/{id}", middleware.UpdateStockById).Methods("PUT", "OPTIONS")
+
+	r.HandleFunc("/api/stock/{id}", middleware.DeleteStockById).Methods("DELETE", "OPTIONS")
+}
