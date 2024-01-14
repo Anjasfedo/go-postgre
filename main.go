23dcfb9ab1b2b0bbd2d2b62db5e96907adad6526
AnjasFedo <fedoaf98@gmail.com>
2024-01-14 11:09:10 +0700

    Create Model to Represent the Table

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/Anjasfedo/go-postgres/routers"
 )
 
 func main() {
+	r := routers.Router()
 
+	fmt.Println("Start Server on Port 8000")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
